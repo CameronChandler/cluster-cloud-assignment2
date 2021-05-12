e257a10523cd15219d469c59c53072d1e19c8e6f
Alastair Daivis <alaroldai@gmail.com>
2021-05-12 22:47:52 +1000

    Make deployment to shared Nectar project work

--- docker/couch-no-stack/up.bash
+++ docker/couch-no-stack/up.bash
@@ -19,7 +19,7 @@ sleep 10
 
 for node in ${othernodes} 
 do
-    curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
+    curl --noproxy ${masternode} -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
       --header "Content-Type: application/json"\
       --data "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\",\
              \"username\": \"${user}\", \"password\":\"${pass}\", \"port\": \"5984\",\
@@ -29,15 +29,15 @@ done
 
 for node in ${othernodes}
 do
-    curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup"\
+    curl --noproxy ${masternode} -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup"\
       --header "Content-Type: application/json"\
       --data "{\"action\": \"add_node\", \"host\":\"${node}\",\
              \"port\": \"5984\", \"username\": \"${user}\", \"password\":\"${pass}\"}"
 done
 
-curl -XGET "http://${user}:${pass}@${masternode}:5984/"
+curl --noproxy ${masternode} -XGET "http://${user}:${pass}@${masternode}:5984/"
 
-curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup"\
+curl --noproxy ${masternode} -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup"\
     --header "Content-Type: application/json" --data "{\"action\": \"finish_cluster\"}"
 
-for node in "${nodes[@]}"; do  curl -X GET "http://${user}:${pass}@${node}:5984/_membership"; done
+for node in "${nodes[@]}"; do  curl --noproxy ${node} -X GET "http://${user}:${pass}@${node}:5984/_membership"; done
