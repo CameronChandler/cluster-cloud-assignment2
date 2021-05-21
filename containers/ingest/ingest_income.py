dea6adc10d668568696c4a438e9874d00e2fab82
limyish <limy8@student.unimelb.edu.au>
2021-05-21 16:12:25 +1000

    add income and employment ingest

---
+++ containers/ingest/ingest_income.py
@@ -0,0 +1,19 @@
+import couchdb
+import json
+
+# set up connection to remote couchdb
+remote_couch = couchdb.Server('https://admin:answering_railcar@118.138.238.242:6984/')
+remote_couch.resource.session.disable_ssl_verification()
+
+# ingest income data
+try:
+    db = remote_couch.create('aurin_income')
+
+except Exception as e:
+    remote_couch.delete('aurin_income')
+    db = remote_couch.create('aurin_income')
+
+with open('res/data_income.json', encoding='utf8') as f:
+    incomes = json.load(f)["features"]
+    for income in incomes:
+        db.save(income)
