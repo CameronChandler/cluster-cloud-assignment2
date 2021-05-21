5bdbfb69c6f8569a076d819cf56a7cdba1decc8c
Alastair Daivis <alaroldai@gmail.com>
2021-05-21 15:56:59 +1000

    make both versions of app.py consistent

--- api/app.py
+++ api/app.py
@@ -7,8 +7,10 @@ import couchdb
 
 import json
 
-# couch = couchdb.Server('http://localhost:5984')
-# db = couch.create('db_test')
+remote_couch = couchdb.Server('https://admin:answering_railcar@118.138.238.242:6984/')
+remote_couch.resource.session.disable_ssl_verification()
+db = remote_couch['db_small_twitter']
+
 
 app = Flask(__name__)
 blueprint = Blueprint('api', __name__, url_prefix='/api')
@@ -44,28 +46,35 @@ class Language(Resource):
 class Database:
 
     def __init__(self):
-        self.data = {
-            'word_lengths_by_city': {
-                "melbourne": 3.3,
-                "adelaide": 2.7,
-                "sydney": 4.2,
-                "darwin": 1.8,
-                "perth": 5.3,
-            },
-
-            'unemployment_pct_by_city': {
+        self.data = {'unemployment_pct_by_city': {
                 "melbourne": 1.0,
                 "adelaide": 2.0,
                 "sydney": 4.0,
                 "darwin": 3.0,
                 "perth": 5.0,
-            }
+            },
+        'word_lengths_by_city': self.get_word_lengths_by_city()
         }
     
+    def get_word_lengths_by_city(self):
+        cities = {}
+        word_lengths_by_city = {}
+        for item in db.view('wordLengths/new-view', group=True):
+            if item.key[0] in cities:
+                cities[item.key[0]]['total_word_length'] += item.value * item.key[1]
+                cities[item.key[0]]['tweet_count'] += item.value
+            else:
+                cities[item.key[0]] = {'total_word_length': item.value * item.key[1], 'tweet_count': item.value}
+
+        for city_name in cities.keys():
+            word_lengths_by_city[city_name] = cities[city_name]['total_word_length'] / cities[city_name]['tweet_count']
+        
+        return word_lengths_by_city
+
     def fetch_view(self, view: str):
         return self.data[view] if view in self.data else None
 
-db = Database()
+local_db = Database()
 
 def fetch_tag(tag, datasets):
     return tag
@@ -82,8 +91,8 @@ def testdata():
 
     # http://haliax.local:5001/testdata?xattr=foo&yattr=bar&tag=a&tag=b&tag=c
 
-    xdata = db.fetch_view(xattr)
-    ydata = db.fetch_view(yattr)
+    xdata = local_db.fetch_view(xattr)
+    ydata = local_db.fetch_view(yattr)
 
     data = [
         {
