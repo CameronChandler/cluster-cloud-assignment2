8309e8642a70e4f03e450bcc11ae437b8be0a418
My Tien Hinh <tienhinh@gmail.com>
2021-05-21 15:50:34 +1000

    Added aurin data

--- docker/stack/api/app/app.py
+++ docker/stack/api/app/app.py
@@ -1,65 +1,39 @@
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 from flask import Flask, Blueprint, jsonify, request
-from flask_restplus import Api, Resource, fields
 from werkzeug.utils import cached_property
 import couchdb
 
-import json
-
 remote_couch = couchdb.Server('https://admin:answering_railcar@118.138.238.242:6984/')
 remote_couch.resource.session.disable_ssl_verification()
-db = remote_couch['db_small_twitter']
-
+twitter_db = remote_couch['db_small_twitter']
+income_db = remote_couch['aurin_income']
+employment_db = remote_couch['aurin_employment']
 
 app = Flask(__name__)
-blueprint = Blueprint('api', __name__, url_prefix='/api')
-api = Api(blueprint, doc='/documentation') #,doc=False
-
-app.register_blueprint(blueprint)
-
-app.config['SWAGGER_UI_JSONEDITOR'] = True
-
-a_language = api.model('Language', {'language' : fields.String('The language.')}) #, 'id' : fields.Integer('ID')
- 
-languages = []
-python = {'language' : 'Python', 'id' : 1}
-languages.append(python)
-
-@api.route('/language')
-class Language(Resource):
-
-    @api.marshal_with(a_language, envelope='the_data')
-    def get(self):
-        # all_languages = db.view('_all_docs', include_docs=True)
-        print('got a get request')
-        return 'ok'
-
-    @api.expect(a_language)
-    def post(self):
-        new_language = api.payload 
-        # db.save(new_language)
-        #new_language['id'] = len(languages) + 1
-        #languages.append(new_language)
-        return {'result' : 'Language added'}, 201 
 
 class Database:
 
     def __init__(self):
-        self.data = {'unemployment_pct_by_city': {
-                "melbourne": 1.0,
-                "adelaide": 2.0,
-                "sydney": 4.0,
-                "darwin": 3.0,
-                "perth": 5.0,
-            },
-        'word_lengths_by_city': self.get_word_lengths_by_city()
+        self.data = {
+            'income_by_city': self.get_income_by_city(),
+            'word_lengths_by_city': self.get_word_lengths_by_city(),
+            'non_school_by_city': self.get_non_school_by_city(),
+            'unemployment_by_city': self.get_unemployment_by_city()
         }
+
+    def translate_city_name(self, name):
+        name_translations = {'Greater Melbourne': 'melbourne', 'Greater Sydney': 'sydney', 'Greater Adelaide': 'adelaide',\
+                            'Greater Brisbane': 'brisbane', 'Greater Hobart': 'hobart', 'Greater Darwin': 'darwin'}
+        if name in name_translations:
+            return name_translations[name]
+        else:
+            return name
     
     def get_word_lengths_by_city(self):
         cities = {}
         word_lengths_by_city = {}
-        for item in db.view('wordLengths/new-view', group=True):
+        for item in twitter_db.view('wordLengths/new-view', group=True):
             if item.key[0] in cities:
                 cities[item.key[0]]['total_word_length'] += item.value * item.key[1]
                 cities[item.key[0]]['tweet_count'] += item.value
@@ -71,6 +45,30 @@ class Database:
         
         return word_lengths_by_city
 
+    def get_income_by_city(self):
+        cities = {}
+        for item in income_db.view('median_income/income-view'):
+            city_name = self.translate_city_name(item.key)
+            if city_name not in cities:
+                cities[city_name] = item.value
+        return cities
+
+    def get_non_school_by_city(self):
+        cities = {}
+        for item in employment_db.view('education/non-school'):
+            city_name = self.translate_city_name(item.key)
+            if city_name not in cities:
+                cities[city_name] = item.value
+        return cities
+
+    def get_unemployment_by_city(self):
+        cities = {}
+        for item in employment_db.view('employment/unemployment-rate'):
+            city_name = self.translate_city_name(item.key)
+            if city_name not in cities:
+                cities[city_name] = item.value
+        return cities
+
     def fetch_view(self, view: str):
         return self.data[view] if view in self.data else None
 
@@ -81,8 +79,8 @@ def fetch_tag(tag, datasets):
 
 @app.route('/testdata')
 def testdata():
-    xattr = request.args.get('xattr') or 'unemployment_pct_by_city'
-    yattr = request.args.get('yattr') or 'word_lengths_by_city'
+    xattr = request.args.get('xattr') or 'income_by_city'
+    yattr = request.args.get('yattr') or 'non_school_by_city' #'word_lengths_by_city'
     tags = request.args.get('tag') or ['city_name']
     
     print(f'xattr: {xattr}')
@@ -97,7 +95,7 @@ def testdata():
     data = [
         {
             'tags': {
-                tag : fetch_tag(tag, { xattr: xdata[city], yattr: ydata[city] })
+                tag : city
                 for tag in tags
             },
             'x': xdata[city],
